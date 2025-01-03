4c2ca40d0cf0b17d93bb0b1f91322ebd99c78bb5
Michael Jurasovic <michael.jurasovic@orkestra.energy>
2025-01-04 10:56:54 +1100

    Remove `db_echo` option
    It unconditionally puts stuff on stdout which messes with MCP's use of stdout https://docs.sqlalchemy.org/en/20/core/engines.html#more-on-the-echo-flag

--- mcpunk/settings.py
+++ mcpunk/settings.py
@@ -9,9 +9,6 @@ class Settings(BaseSettings):
     # SQLite database path
     db_path: Path = Path("~/.mcpunk/db.sqlite").expanduser()
 
-    # Enable SQLAlchemy query logging
-    db_echo: bool = True
-
     enable_log_file: bool = True
     log_file: Path = Path("~/.mcpunk/mcpunk.log").expanduser()
     log_level: Literal["DEBUG", "INFO", "WARNING", "ERROR", "FATAL", "CRITICAL"] = "DEBUG"
